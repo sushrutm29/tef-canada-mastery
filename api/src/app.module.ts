d6bb222c54c3d508c15d32a6e91787035964d735
sushrutm29 <sushrutmdv761@gmail.com>
2026-01-12 01:16:34 -0500

    minor comment changes to test github actions

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -11,11 +11,11 @@ import * as fs from 'fs';
     TypeOrmModule.forRoot({  // Configures database connection
       type: 'postgres',      // Database type
       host: process.env.DB_HOST,     // RDS endpoint from .env
-      port: parseInt(process.env.DB_PORT || '5432'),  // Convert string to number
+      port: parseInt(process.env.DB_PORT || '5432'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      autoLoadEntities: true,  // Automatically finds entity files
+      autoLoadEntities: true,  // Automatically finds entity files and updates db accordingly
       synchronize: process.env.NODE_ENV !== 'production',      // Auto-creates tables (DANGER in production - only for dev)
       ssl: process.env.DB_SSL === 'true' ? {
         ca: fs.readFileSync('./global-bundle.pem').toString()
